87b097fc740e56e8a41700ca3146ad34472a7b24
Chris Tickner <chris.tickner@gmail.com>
2024-11-10 21:22:05 -0400

    Update smeedish.WIP.php v2
    Signed-off-by: Chris Tickner <chris.tickner@gmail.com>

--- smeedish.WIP.php
+++ smeedish.WIP.php
@@ -1,58 +1,63 @@
 <?php
 
-namespace Smeed\Smeedish;
+require 'vendor/autoload.php';
 
 use Symfony\Component\String\UnicodeString;
-use Symfony\Component\DependencyInjection\ContainerAwareTrait;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
-use Symfony\Contracts\Service\ServiceSubscriberInterface;
-use Symfony\Component\DependencyInjection\ServiceSubscriberTrait;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class EncryptionService
+class SmeedishEncryption
 {
-    use ContainerAwareTrait;
-    use ServiceSubscriberTrait;
-
-    private string $encryptionKey;
-
-    public function __construct(string $encryptionKey)
+    public function encrypt(string $input): string
     {
-        $this->encryptionKey = $encryptionKey;
+        $words = preg_split('/\s+/', trim($input));
+
+        if (count($words) === 1) {
+            return $this->encryptSingleWord($words[0]);
+        } else {
+            return $this->encryptMultiWord($words);
+        }
     }
 
-    public function encrypt(string $plainText): string
+    private function encryptSingleWord(string $word): string
     {
-        // Encrypt the given plaintext using OpenSSL
-        $iv = random_bytes(openssl_cipher_iv_length('aes-256-cbc'));
-        $encryptedText = openssl_encrypt($plainText, 'aes-256-cbc', $this->encryptionKey, 0, $iv);
-
-        if ($encryptedText === false) {
-            throw new \RuntimeException('Failed to encrypt data.');
-        }
-
-        return base64_encode($iv . $encryptedText);
+        return $this->addRandomCharacters($word);
     }
 
-    public function decrypt(string $cipherText): string
+    private function encryptMultiWord(array $words): string
     {
-        $decodedCipherText = base64_decode($cipherText);
-        $ivLength = openssl_cipher_iv_length('aes-256-cbc');
-        $iv = substr($decodedCipherText, 0, $ivLength);
-        $encryptedText = substr($decodedCipherText, $ivLength);
+        $filteredWords = array_map(function ($word) {
+            return preg_replace('/[^a-z]/', '', $word);
+        }, $words);
 
-        $plainText = openssl_decrypt($encryptedText, 'aes-256-cbc', $this->encryptionKey, 0, $iv);
+        $filteredText = implode(' ', $filteredWords);
+        return $this->addRandomCharacters($filteredText);
+    }
 
-        if ($plainText === false) {
-            throw new \RuntimeException('Failed to decrypt data.');
+    private function addRandomCharacters(string $text): string
+    {
+        $characters = str_split($text);
+        $encryptedCharacters = [];
+
+        foreach ($characters as $char) {
+            $encryptedCharacters[] = $char;
+            if (ctype_alpha($char)) {
+                $randomChar = $this->getRandomCharacter();
+                $encryptedCharacters[] = $randomChar;
+            }
         }
 
-        return $plainText;
+        return implode('', $encryptedCharacters);
+    }
+
+    private function getRandomCharacter(): string
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ#@$%&*';
+        return $characters[random_int(0, strlen($characters) - 1)];
     }
 }
 
-// Usage example:
-// $encryptionService = new EncryptionService('your-secret-key');
-// $encrypted = $encryptionService->encrypt('Hello, World!');
-// $decrypted = $encryptionService->decrypt($encrypted);
+$input = "hello world! This is smeedish encryption.";
+$smeedishEncryption = new SmeedishEncryption();
+$encryptedText = $smeedishEncryption->encrypt($input);
+
+echo "Original Text: " . $input . PHP_EOL;
+echo "Encrypted Text: " . $encryptedText . PHP_EOL;
